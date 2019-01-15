18fb5ed7b6354dda57f11add19bd5acbc7383e30
dkati <dkatiridis@gmail.com>
2019-01-15 14:07:16 +0100

    jfltexx: We use OSS RIL now
    * Don't need these arguments.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,9 +26,6 @@ include device/samsung/jf-common/BoardConfigCommon.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := GT-I9505,GT-I9505G,SGH-I337M,SGH-M919,I9505,I9505G,I337M,M919,i9505,i9505g,i337m,m919,jflte,jfltecan,jfltetmo,jfltexx,jgedlte
 
-# Properties
-TARGET_SYSTEM_PROP += device/samsung/jfltexx/system.prop
-
 # Vendor Init
 TARGET_INIT_VENDOR_LIB := libinit_jfltexx
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/jfltexx/init/init_jfltexx.cpp
